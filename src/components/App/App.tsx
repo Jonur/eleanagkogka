64bc20f35015e9a0c3741ea91fe2182325fcdb16
Dimitris Damilos <d.k.damilos@gmail.com>
2024-05-04 16:01:15 +0100

    Setup fonts

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App: React.FC = () => {
-  return <div className="text-red-100">Eleana Gkogka2</div>;
+  return <div className="p-0">Eleana Gkogka2</div>;
 };
 
 export default App;
