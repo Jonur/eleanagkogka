9a3c19d870a79669dc5d088e9202d0507965f161
Dimitris Damilos <d.k.damilos@gmail.com>
2024-05-04 16:22:48 +0100

    Add routes

--- src/components/App/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const App: React.FC = () => {
-  return <div className="p-0">Eleana Gkogka2</div>;
-};
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
 import './index.css';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './components/Root';
+import About from './components/About';
+import Resume from './components/Resume';
+import Work from './components/Work';
+import ErrorPage from './components/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+  {
+    path: '/resume',
+    element: <Resume />,
+  },
+  {
+    path: '/work',
+    element: <Work />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
