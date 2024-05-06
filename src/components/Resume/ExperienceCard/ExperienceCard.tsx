d79a95b1557a324deffdf5a859a95ff0f1044845
Dimitris Damilos <d.k.damilos@gmail.com>
2024-05-06 23:55:59 +0100

    Finalise Mobile experience

--- src/components/Resume/ExperienceCard/ExperienceCard.tsx
+++ src/components/Resume/ExperienceCard/ExperienceCard.tsx
@@ -1,13 +1,27 @@
 import c from 'classnames';
-import React from 'react';
+import React, { useState } from 'react';
 
+import { useOnMount } from 'src/hooks';
 import { Experience } from 'src/types';
 
-type ExpericeProps = {
+type ExperienceProps = {
   experience: Experience;
 };
 
-const ExperienceCard: React.FC<ExpericeProps> = ({ experience: { title, where, when, description, pointColor } }) => {
+const ExperienceCard: React.FC<ExperienceProps> = ({ experience: { title, where, when, description, pointColor } }) => {
+  const [collapsed, setCollapsed] = useState(true);
+  const [preview, setPreview] = useState('');
+
+  useOnMount(() => {
+    const firstParagraph =
+      description.indexOf('<p>') === 0 ? description.substring(3, description.indexOf('</p>')) : description;
+    setPreview(firstParagraph);
+  });
+
+  const toggleExperience = () => {
+    setCollapsed((prev) => !prev);
+  };
+
   return (
     <div className="relative border-l border-black border-opacity-15 pl-6 [&:not(:last-child)]:pb-8">
       <div className={c('absolute w-4 h-4 rounded-full -left-2', pointColor)} />
@@ -16,10 +30,35 @@ const ExperienceCard: React.FC<ExpericeProps> = ({ experience: { title, where, w
       <h3 className="text-base leading-[22px] lg:text-lg lg:leading-7 uppercase mb-1">
         @ <span className="font-bold">{where}</span>
       </h3>
-      <div
-        className="leading-6 lg:leading-7 lg:text-lg flex flex-col gap-y-2"
-        dangerouslySetInnerHTML={{ __html: description }}
-      />
+
+      {collapsed ? (
+        <div className="text-clipped-preview text-base leading-6 lg:text-lg lg:leading-7 h-[66px]">
+          <span>{preview}</span>
+          <button
+            onClick={toggleExperience}
+            className="-bottom-[5px] right-[2px] text-clipped-preview-link text-teal-dark leading-6 lg:leading-7 lg:text-lg text-base underline"
+          >
+            View more
+          </button>
+        </div>
+      ) : (
+        <>
+          <div
+            className="leading-6 lg:leading-7 lg:text-lg flex flex-col gap-y-2"
+            dangerouslySetInnerHTML={{
+              __html: description,
+            }}
+          />
+          <div className="flex justify-end">
+            <button
+              onClick={toggleExperience}
+              className="-bottom-[5px] right-[2px] mt-2 text-teal-dark leading-5 lg:leading-7 lg:text-lg text-base underline"
+            >
+              View less
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
