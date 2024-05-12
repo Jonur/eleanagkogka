ac8411fb2610570457031cc312c68b5e86bf652c
Dimitris Damilos <d.k.damilos@gmail.com>
2024-05-12 13:11:56 +0100

    Serious progress on Case Studies

--- src/components/Work/CaseStudy/CaseStudy.tsx
+++ src/components/Work/CaseStudy/CaseStudy.tsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import Contact from 'src/components/Contact';
 import Footer from 'src/components/Footer';
 import Header from 'src/components/Header';
+import { useWindowDimensions } from 'src/hooks';
 import { Route } from 'src/types';
 
 import { CASE_STUDIES } from '../constants';
@@ -13,14 +14,54 @@ const CaseStudy: React.FC = () => {
   const { caseStudyId = '' } = useParams();
   const caseStudy = CASE_STUDIES[caseStudyId];
 
+  const { onLargeScreen } = useWindowDimensions();
+
   return caseStudy ? (
     <>
       <Header pageHeader={caseStudy.title} breadcrumb={{ to: Route.WORK, label: 'Work' }} />
 
-      <div className="pt-[60px] lg:pt-[100px] px-6 pb-4 flex flex-col items-center gap-y-[60px] lg:gap-y-[100px]">
-        <section className="text-dark-blue max-w-[1180px] w-full lg:flex lg:justify-between">
-          <h2 className="text-3xl lg:text-[32px] font-bebas uppercase mb-8">{caseStudy.title}</h2>
-        </section>
+      <div className="pt-[60px] lg:pt-[100px] px-6 pb-4 flex flex-col items-center gap-y-[60px] lg:gap-y-[80px] lg:last:mb-[20px]">
+        {(caseStudy.sections || []).map((section, index) => {
+          const key = `${section.title}-${index}`;
+          return (
+            <section key={key} className="text-dark-blue max-w-[1180px] w-full lg:flex lg:justify-between">
+              <h2 className="text-3xl lg:text-[32px] font-bebas uppercase mb-4 lg:mb-8">{section.title}</h2>
+
+              <div className="flex flex-col lg:flex-1 lg:max-w-[880px] gap-y-6">
+                {section.entries.map((entry) => {
+                  const shouldAddLabel = onLargeScreen() || (!onLargeScreen() && 'label' in entry);
+
+                  return (
+                    <div
+                      key={crypto.randomUUID()}
+                      className="flex-1 flex flex-col gap-y-4 lg:gap-y-0 lg:flex-row lg:gap-x-10"
+                    >
+                      {shouldAddLabel && (
+                        <div className="lg:w-[140px] lg:text-right shrink-0 text-base leading-[22px] lg:text-lg lg:leading-7 uppercase font-bold">
+                          {'label' in entry ? entry.label : ''}
+                        </div>
+                      )}
+                      <div className="flex flex-col gap-y-4 lg:gap-y-6 lg:max-w-[700px]">
+                        {'title' in entry && (
+                          <h3 className="text-2xl lg:text-[26px] italic font-light">{entry.title}</h3>
+                        )}
+                        {'content' in entry && (
+                          <span
+                            className="flex flex-col gap-y-2 leading-6 lg:leading-7 lg:text-lg [&_a]:text-teal-dark [&_a]:border-b [&_a]:border-teal-dark [&_a]:inline-block [&_a]:leading-[13px]"
+                            dangerouslySetInnerHTML={{
+                              __html: entry.content,
+                            }}
+                          />
+                        )}
+                        {'image' in entry && <img src={entry.image} className="w-full h-auto" />}
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </section>
+          );
+        })}
 
         <SocialProfiles />
 
--- src/components/Work/constants.ts
+++ src/components/Work/constants.ts
@@ -13,15 +13,74 @@ export const CASE_STUDIES: Record<CaseStudy['id'], CaseStudy> = {
     sections: [
       {
         title: 'Introduction',
-        entries: [],
+        entries: [
+          {
+            title: 'In this first part of the Onboarding Project case study...',
+            content:
+              'I’ll share the project background, detailing how I set goals and prioritised tasks using various data sources. You’ll gain insights into my design thinking process, strategies for user research and conducting interviews, and how I collaborate effectively with teams. Additionally, you’ll get a preview of some of the design solutions and artefacts developed throughout this project, including a look at different A/B tests from inception to conclusion.',
+          },
+          {
+            label: 'The product',
+            content:
+              'StreamYard is a professional live streaming and recording browser based studio. With SY you can record, go live on your website or multiple platforms like Youtube and Facebook at once.',
+          },
+          {
+            label: 'My role',
+            content: 'Product Design Lead  &bull;  Designer  &bull;  Researcher',
+          },
+          {
+            content:
+              '<em>This case study covers the first phase of the onboarding project, starting on August 2023.</em>',
+          },
+        ],
       },
       {
         title: 'Background',
-        entries: [],
+        entries: [
+          {
+            title:
+              'It all began with an amalgamation of customer observations, compelling data, a colleague’s request for support, and a designer’s (yours truly) ambition to lead a high-impact project. An opportunity was presented to align user-centred design with StreamYard’s strategic business goals.',
+          },
+          {
+            label: 'Observations and data points',
+            content: `
+              <p><strong>Evolving Use Cases:</strong> Initially designed for streamers, StreamYard now accommodates streaming, recording, and webinars. This expansion isn't fully mirrored in the app’s current design, leading to a disconnect with user expectations and needs.</p>
+              <p><strong>Increased Complexity:</strong> Introducing new features has enriched but unintentionally complicated the product, making it harder for new users to navigate and fully utilise its functionalities.</p>
+              <p><strong>User Engagement Decline:</strong> Although enhancements like the Google login have boosted sign-ups by nearly 10%, there’s been a drop in new user broadcasts, a critical metric for assessing long-term engagement, indicating the post-sign-up flows aren’t fully optimised.</p>
+              <p><strong>User Experience Issues:</strong> Observing new users through Fullstory sessions has revealed usability barriers, hesitation or confusion within the Dashboard and Studio, which harms engagement.</p>
+            `,
+          },
+          {
+            label: 'Team struggles',
+            content:
+              'A colleague from the Studio product team highlighted difficulties in balancing feature delivery with supporting customers through effective onboarding, leading to an overload of messages that lack hierarchy or focus and often distract rather than help users.',
+          },
+          {
+            label: 'Business goals',
+            content:
+              'The Growth team’s primary objective was to reignite top-of-funnel growth by optimising acquisition, activation, and adoption while focusing on enhancing and optimising user journeys.',
+          },
+          {
+            label: 'The opportunity',
+            content:
+              'The alignment of StreamYard’s goals with the observed challenges presented an excellent opportunity to overhaul the onboarding experience. A more consistent, supportive, and tailored onboarding system could more effectively guide users through the product, helping them discover the full value of StreamYard.',
+          },
+        ],
       },
       {
         title: 'Getting started',
-        entries: [],
+        entries: [
+          {
+            title:
+              'Being aware that effective onboarding is crucial to a product’s success, I was eager to initiate this project. The goal was to develop a relevant and cohesive onboarding system to support new users in reaching their AHA moments while fostering long-term engagement and satisfaction.',
+          },
+          {
+            label: 'The hypothesis',
+            content: `
+              <p>An onboarding system will provide guidance and support to keep our customers engaged and motivated to explore and discover StreamYard features, reaching their content creation goals. It will help us communicate and deliver the best value for our customers’ wants and needs. This, in turn, will boost customer acquisition and reduce churn.</p>
+              <p>This hypothesis will help us communicate the project value to stakeholders while serving as our north star. &ndash; <a href="#">More about on how I form a hypothesis</a>.</p>`,
+          },
+        ],
       },
     ],
   },
