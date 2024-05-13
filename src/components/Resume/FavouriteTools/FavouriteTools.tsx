f82d0a2d9a19d21e1830c6421f4630d19ff332f4
Dimitris Damilos <d.k.damilos@gmail.com>
2024-05-13 21:09:24 +0100

    logo offset fix

--- src/components/Resume/FavouriteTools/FavouriteTools.tsx
+++ src/components/Resume/FavouriteTools/FavouriteTools.tsx
@@ -1,18 +1,61 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
-import { useWindowDimensions } from 'src/hooks';
+import { useOnMount, useWindowDimensions } from 'src/hooks';
 
 const FavouriteTools: React.FC = () => {
   const { onLargeScreen } = useWindowDimensions();
+  const sectionRef = useRef<HTMLDivElement>(null);
+
+  const [offsetLeft, setOffsetLeft] = useState(0);
+  useOnMount(() => {
+    let resizeObserverEntries: ResizeObserverEntry[] = [];
+    const observer = new ResizeObserver((entries) => {
+      resizeObserverEntries = entries;
+      for (const entry of entries) {
+        if ('offsetLeft' in entry.target) {
+          setOffsetLeft(Number(entry.target.offsetLeft));
+        }
+      }
+    });
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current);
+    }
+
+    return () => {
+      resizeObserverEntries.forEach((entry) => entry.target.remove());
+      observer.disconnect();
+    };
+  });
 
   return (
-    <section className="border-t border-b border-black border-opacity-15 pt-3 lg:pt-4 pb-[114px] lg:pb-[100px] text-dark-blue max-w-[1180px] w-full relative">
-      <h3 className="text-lg italic mb-12 lg:mb-[80px]">Favourite tools</h3>
+    <section
+      ref={sectionRef}
+      className="border-t border-b border-black border-opacity-15 pt-3 lg:pt-4 pb-[114px] lg:pb-[100px] text-dark-blue max-w-[1180px] w-full relative"
+    >
+      <h3 className="text-lg italic mb-12 lg:mb-[80px] relative z-20 lg:z-auto">Favourite tools</h3>
 
       <div
-        className="overflow-hidden flex gap-x-10 absolute lg:static -left-6 h-[34px]"
+        className="bg-gradient-to-r from-transparent from-[-10%] lg:from-[5%] to-white opacity-70 absolute -right-1 h-full top-0 z-10"
         style={{
-          width: onLargeScreen() ? '100%' : `${window.outerWidth}px`,
+          width: `${(offsetLeft || 0) + 40}px`,
+          right: onLargeScreen() ? `-${offsetLeft || 0}px` : '-24px',
+        }}
+      />
+
+      <div
+        className="bg-gradient-to-r from-transparent from-[-10%] lg:from-[5%] to-white opacity-70 absolute -left-1 h-full top-0 z-10 rotate-180"
+        style={{
+          width: `${(offsetLeft || 0) + 40}px`,
+          left: onLargeScreen() ? `-${offsetLeft || 0}px` : '-24px',
+        }}
+      />
+
+      <div
+        className="overflow-hidden flex gap-x-10 absolute -left-6 h-[34px]"
+        style={{
+          width: `${window.outerWidth}px`,
+          left: `-${offsetLeft || 0}px`,
         }}
       >
         <div className="flex gap-x-10 lg:gap-x-8 shrink-0 items-center justify-around min-w-full animate-marquee">
