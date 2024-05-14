63336a2ba9e864347a609d80e7c8b407980cb29d
Dimitris Damilos <d.k.damilos@gmail.com>
2024-05-14 12:44:12 +0100

    Add testimonials

--- src/components/Resume/Testimonials/constants.ts
+++ src/components/Resume/Testimonials/constants.ts
@@ -17,17 +17,38 @@ export const TESTIMONIALS: Testimonial[] = [
   },
   {
     passage:
-      '... Eleana has excelled in leading the growth team’s development of detailed user research. Eleana developed highly effective user interview techniques which not only helped in our understanding of the user, but directly fed additional A/B test ideas into the team...',
-    author: 'Nicolas Taylor',
-    jobTitle: 'Product manager',
+      '...Eleana is creative and thoughtful. She’s a driver. She’s persistent, strong-willed, but also very flexible and empathetic. She knows how to convey complex ideas, present concepts to product partners, and facilitate conversations with product teams, with clarity... ',
+    author: 'Noah Shrader',
+    jobTitle: 'Head of design',
     company: 'Streamyard',
   },
   {
     passage:
-      'A fantastic overview of gestalt principles for better visual communication, explaining how people perceive and connect visual objects and patterns — from closure and symmetry to continuation and proximity... by Eleana Gkogka',
-    author: 'Martin Friedman',
-    jobTitle: 'Founder of',
-    company: 'Smashing magazine',
+      '...Eleana’s contributions have undeniably bolstered the growth of both the product and the company. Eleana’s strategic thinking and dedication to data-driven design have consistently yielded tangible results, making her an invaluable asset to any team...',
+    author: 'Will Brett-Atkin',
+    jobTitle: 'Lead product designer',
+    company: 'Streamyard',
+  },
+  {
+    passage:
+      'A great designer and a fantastic communicator come to mind when thinking of Eleana... Above all, I was impressed with her ability to influence product and design decisions – perfectly balancing user needs and business goals...',
+    author: 'Daryl Alexander',
+    jobTitle: 'UX researcher',
+    company: 'Gain Credit',
+  },
+  {
+    passage:
+      'Eleana is a considered and pragmatic product designer... She always shows great awareness of the customer, with a flair for micro animations, design system implementation and builds strong relationships with her stakeholders.',
+    author: 'Stuart Pearce',
+    jobTitle: 'Senior product designer',
+    company: 'Experian',
+  },
+  {
+    passage:
+      'Working with Eleana has always been refreshing and eye-opening. As a product designer, Eleana brings solid behavioural science knowledge that informs her hypothesis, making sure that her design decisions are well thought through...',
+    author: 'Karolina Kubski',
+    jobTitle: 'Product designer',
+    company: 'Experian',
   },
 ];
 
